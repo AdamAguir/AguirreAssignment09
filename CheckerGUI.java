3eee448f3d3b5bc5516179c22cc228b135fe19da
Adam Aguirre <aethershade@live.com>
2021-04-05 14:18:59 -0600

    Initial push need to fix the paintComponet method

---
+++ CheckerGUI.java
@@ -0,0 +1,55 @@
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.Image;
+
+
+public class CheckerGUI extends JFrame{
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Checker Board");
+        frame.setSize(800,800);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Image icon = new ImageIcon("res/Jedi_Order.JPG").getImage();
+        frame.setIconImage(icon);
+
+        Color light = new Color(130,12,12);
+        Color dark = new Color(0,0,0);
+
+        Container pane = frame.getContentPane();
+        GridLayout grid = new GridLayout(8,8);
+        pane.setLayout(grid); // Need to set layout to add multiple Jpanels using GridLayout for the checkerboard
+        
+        
+
+        // make the CheckerPanels
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i % 2 == 0) {
+                    if (j % 2 == 0) {
+                       pane.add(new CheckerPanel(light, i, false));
+                    } else {
+                       pane.add(new CheckerPanel(dark, i, true));
+
+                    }
+                } else {
+                    if (j % 2 == 0) {
+                        pane.add(new CheckerPanel(dark, i, true));
+                    } else {
+                        pane.add(new CheckerPanel(light, i, false));
+                    }
+                }
+            }
+        }
+        // Pack the checker panels into the frame
+        frame.pack();
+    }
+ 
+}
---
+++ CheckerPanel.java
@@ -0,0 +1,46 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
+
+public class CheckerPanel extends JPanel{
+    private BufferedImage img = new BufferedImage(85, 85, BufferedImage.TYPE_INT_ARGB);
+    //private BufferedImage lightPiece = new BufferedImage(85, 85, BufferedImage.TYPE_INT_ARGB);//ImageIO.read(new File("res/Rebel.jpeg"));
+    private Image darkPiece = new ImageIcon("res/Imperial.jpeg").getImage();
+    private ImageIcon lightP = new ImageIcon("res/Rebel.jpeg");
+    private Image lightPiece = new ImageIcon("res/Rebel.jpeg").getImage();
+    private Graphics g = img.createGraphics();
+    private int x = darkPiece.getWidth(null)/2;
+    private int y = darkPiece.getHeight(null)/2;
+
+    public CheckerPanel(Color c, int row, boolean paint){
+        //super();
+        setBackground(c);
+        setPreferredSize(new Dimension(100, 100));
+        // if (paint) {
+        //     System.out.println(row);
+        //     paintComponet(g, row);
+        // }
+         //paintComponent(g);
+    }
+
+    public  void paintComponet(Graphics g){
+        super.paintComponent(g);
+        this.setBackground(Color.WHITE);
+
+        g.setColor(Color.CYAN);
+        g.drawOval(50, 50, 25, 25);
+            //g.drawImage(darkPiece, x, y, null);
+        lightP.paintIcon(this, g, x, y);
+            //g.drawImage(lightPiece, x, y, null);
+        
+    }
+}
