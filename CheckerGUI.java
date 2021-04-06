30af54c8af895d98ca4dc861d0a7e0b427bf0194
Adam Aguirre <aethershade@live.com>
2021-04-06 00:41:52 -0600

    Added comments and cleaned up code

--- CheckerGUI.java
+++ CheckerGUI.java
@@ -6,51 +6,55 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.Image;
 
-public class CheckerGUI extends JFrame{
+/**
+ * @author: Adam Aguirre 
+ * Assignment 9 
+ * 04/06/2021
+ */
+public class CheckerGUI extends JFrame {
 
-    public CheckerGUI(){
-            setTitle("Checker Board");
-            setUndecorated(false);
-            Image icon = new ImageIcon("res/Jedi_Order.JPG").getImage();
-            setIconImage(icon);            
-            
-            Container pane = getContentPane();
-            
-            GridLayout grid = new GridLayout(8,8);
-            pane.setLayout(grid); // Need to set layout to add multiple Jpanels using GridLayout for the checkerboard
-            Color light = new Color(130,12,12);
-            Color dark = new Color(0,0,0);
-            
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    if (i % 2 == 0) {
-                        if (j % 2 == 0) {
+    public CheckerGUI() {
+        setTitle("Checker Board");
+        setUndecorated(false);
+        Image icon = new ImageIcon("res/Jedi_Order.JPG").getImage();
+        setIconImage(icon);
+
+        Container pane = getContentPane();
+
+        GridLayout grid = new GridLayout(8, 8);
+        pane.setLayout(grid); // Need to set layout to add multiple Jpanels using GridLayout for the
+                              // checkerboard
+        Color light = new Color(130, 12, 12);
+        Color dark = new Color(0, 0, 0);
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i % 2 == 0) {
+                    if (j % 2 == 0) {
                         pane.add(new CheckerPanel(light, i, false));
-                        } else {
-                        pane.add(new CheckerPanel(dark, i, true));
-    
-                        }
                     } else {
-                        if (j % 2 == 0) {
-                            pane.add(new CheckerPanel(dark, i, true));
-                        } else {
-                            pane.add(new CheckerPanel(light, i, false));
-                        }
+                        pane.add(new CheckerPanel(dark, i, true));
+
+                    }
+                } else {
+                    if (j % 2 == 0) {
+                        pane.add(new CheckerPanel(dark, i, true));
+                    } else {
+                        pane.add(new CheckerPanel(light, i, false));
                     }
                 }
             }
-            pack();  // use this when using preferredSize in a component
+        }
+        pack(); // use this when using preferredSize in a component
     }
+
     public static void main(String[] args) {
-		
-		
-		EventQueue.invokeLater(() -> 
-		{
+
+        EventQueue.invokeLater(() -> {
             CheckerGUI frame = new CheckerGUI();
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
-			
-			frame.setVisible(true); // always last
-		});
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            frame.setVisible(true); // always last
+        });
     }
 }
-
--- CheckerPanel.java
+++ CheckerPanel.java
@@ -1,46 +1,44 @@
 import java.awt.Color;
 import java.awt.Dimension;
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
+/**
+ * @author: Adam Aguirre 
+ * Assignment 9 
+ * 04/06/2021
+ */
+public class CheckerPanel extends JPanel {
+	private Image darkPiece = new ImageIcon("res/Imperial.jpeg").getImage();
+	private Image lightPiece = new ImageIcon("res/Rebel.jpeg").getImage();
+	private Image img;
 
-
-public class CheckerPanel extends JPanel{
-    private Image darkPiece = new ImageIcon("res/Imperial.jpeg").getImage();
-    private Image lightPiece = new ImageIcon("res/Rebel.jpeg").getImage();
-    private Image img;
-   
-
-    public CheckerPanel(Color c, int row, boolean p){
-        setBackground(c);
-		setPreferredSize(new Dimension(100, 100));	
+	public CheckerPanel(Color c, int row, boolean p) {
+		setBackground(c);
+		setPreferredSize(new Dimension(100, 100));
 		if (p) {
-			if(row < 3){
-				img = darkPiece;		
+			if (row < 3) {
+				img = darkPiece;
 			}
 			if (row > 4) {
-				img = lightPiece;		
+				img = lightPiece;
 			}
 		} else {
 			img = null;
 		}
-    }
+	}
 
-    public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		
+
 		int w = getWidth();
 		int h = getHeight();
-		// need to find the center of the screen by subtracting half the image w/h from screen w/h
-		if (img != null){
+		// need to find the center of the screen by subtracting half the image w/h from
+		// screen w/h
+		if (img != null) {
 			g.drawImage(img, w / 2 - (img.getWidth(null) / 2), h / 2 - (img.getHeight(null) / 2), null);
 		}
-}
+	}
 }
